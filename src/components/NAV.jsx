59ab035b1553f4008ad770ed8ee3513a3d6c1c5a
elkarroumytaha <elkarroumytaha@gmail.com>
2022-04-13 01:37:15 +0000

    init

---
+++ src/components/NAV.jsx
@@ -0,0 +1,23 @@
+
+
+function Nav() {
+  return (
+    <>
+  <nav className="p-2 shadow-md">
+       <div className= "flex flex-row flex-nowrap">
+         <a className="text-lg font-bold m-3 hover:text-cyan-600" href="/">Portfolio</a>
+
+         <a className="text-lg font-bold m-3 hover:text-cyan-600" href="#">Blog</a>
+         <a className="text-lg font-bold m-3 hover:text-cyan-600" href="https://twitter.com/ElkarroumyTaha">Twitter</a>
+         <a className="text-lg font-bold m-3 hover:text-cyan-600" href="https://github.com/t-el">Github</a>
+         <a className="text-lg font-bold m-3 hover:text-cyan-600" href="https://medium.com/@elkarroumytaha">Medium</a>
+
+         
+       </div>
+  </nav>
+
+    </>
+  );
+}
+
+export default Nav;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+     './public/index.html',
+     './src/**/*.{html,js,jsx}',
+   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
